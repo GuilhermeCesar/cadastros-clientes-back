b1ddfefe22db0a42ecbbd0bd69a5116851d11c1e
guilherme <gui11_3@hotmail.com>
2018-12-23 07:58:34 -0200

    Create end point post customer

--- src/main/java/com/essencia/resources/CustomerResouce.java
+++ src/main/java/com/essencia/resources/CustomerResouce.java
@@ -1,6 +1,7 @@
 package com.essencia.resources;
 
 
+import com.essencia.dto.CustomerDto;
 import com.essencia.model.Customer;
 import com.essencia.service.CostumerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +16,23 @@ public class CustomerResouce {
     @Autowired
     private CostumerService costumerService;
 
-
     @CrossOrigin
     @GetMapping
     public @ResponseBody
-    ResponseEntity<?> searchAllProposals(){
+    ResponseEntity<?> searchAllCustomer(){
         try{
             Iterable<Customer> customers = this.costumerService.searchAllProposals();
             return new ResponseEntity<>(customers, HttpStatus.OK);
+        }catch (Exception ex){
+            return new ResponseEntity<>("Erro no servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public @ResponseBody ResponseEntity<?> createCustomer(@RequestBody CustomerDto customerDto){
+        try {
+            this.costumerService.createCustomer(customerDto);
+
+            return new ResponseEntity<>("Salvo com sucesso",HttpStatus.OK);
         }catch (Exception ex){
             return new ResponseEntity<>("Erro no servidor", HttpStatus.INTERNAL_SERVER_ERROR);
         }
